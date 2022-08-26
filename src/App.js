677658fb598d29c27eea93af8c8f84ff2a7755ce
mappuboy <pituvaraliyange@gmail.com>
2022-08-26 11:03:07 +0530

    remove

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.css';
-import SignInOutContainer from './containers/index.js';
+import Login from './components/login'
+import Signup from './components/signup';
 
 
 function App() {
   return (
     <div className="App">
-     <SignInOutContainer/>
-      
+     <Signup/>
     </div>
-  )
+  );
 }
 
 export default App;
--- src/containers/index.js.js
+++
@@ -1,7 +0,0 @@
-import React,{useState} from "react";
-const SignInOutContainer = () =>{
-    return(
-        <h1>signoutin</h1>
-    )
-}
-export default SignInOutContainer;
